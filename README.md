92e37d05f2993ded3197c726c32deca320cc3a48
Alicia Wang <aliwang2001@gmail.com>
2020-01-11 13:28:20 -0800

    Added React files

---
+++ README.md
@@ -0,0 +1,24 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Install
+
+1. `npm install -g yarn`
+2. `yarn install`
+
+#### What is Yarn?
++ https://code.facebook.com/posts/1840075619545360
++ https://scotch.io/tutorials/yarn-package-manager-an-improvement-over-npm
+
+## Run
+
+1. `npm start`
+2. Open `http://localhost:8080` in a web browser.
+
+## Build
+
+`npm run build`
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+
+import { ReactDOM, render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import listItems from './reducers/ListItems';
+import ShoppingListContainer from './components/ShoppingListContainer';
+
+import * as serviceWorker from './serviceWorker';
+
+
+const store = createStore(listItems, compose(window.devToolsExtension ? window.devToolsExtension() : f => f));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+render(
+  <Provider store={store}>
+    <ShoppingListContainer />
+  </Provider>,
+  document.querySelector('[data-react-application]')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
