85c2ce13a5a623aa23084313c0359457983d193d
jojo <xianren5588@gmail.com>
2020-01-12 02:31:18 -0800

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import '../App.css';
+
+class Dropdown extends React.Component {
+constructor(){
+ super();
+
+ this.state = {
+       displayMenu: false,
+     };
+
+  this.showDropdownMenu = this.showDropdownMenu.bind(this);
+  this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+
+};
+
+showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+    document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+
+  }
+
+
+
+  handleClick() {
+    // let clicked=this.state.clicked;
+    //   clicked[n]=!clicked[n]
+    //   this.setState({clicked:clicked});
+    //alert('Hello!');
+    }
+
+  render() {
+    return (
+        <div  className="dropdown" style = {{background:"lightseagreen",width:"200px",borderRadius:"10px",position: "absolute",right: "8%",top: "2%",}} >
+         <div className="button" onClick={this.showDropdownMenu}> Languages </div>
+
+          { this.state.displayMenu ? (
+          <ul>
+         <div className="dropdown-item" ref="megaDrop">
+            <li><a className="dropdown-link" href="#Eng" onClick={this.handleClick.bind()}>English</a>
+             {/* <div className={"mega-menu"+" "+this.state.clicked[1]}>
+              <div className="mega-menu-content">
+                <p>First Menu</p>
+              </div>
+            </div> */}
+            </li>
+          </div>
+         <div className="dropdown-item" ref="megaDrop">
+            <li><a className="dropdown-link" href="#Chn" onClick={this.handleClick.bind()}>Chinese</a>
+            </li>
+         </div>
+         <div className="dropdown-item" ref="megaDrop">
+            <li><a className="dropdown-link" href="#Jpn" onClick={this.handleClick.bind()}>Japanese</a>
+            </li>
+         </div>
+         <div className="dropdown-item" ref="megaDrop">
+            <li><a className="dropdown-link" href="#Spn" onClick={this.handleClick.bind()}>Spanish</a>
+            </li>
+         </div>
+         {/* <li><a href="#Activity Logs">Activity Logs</a></li>
+         <li><a href="#Setting">Setting</a></li>
+         <li><a href="#Log Out">Log Out</a></li> */}
+          </ul>
+        ):
+        (
+          null
+        )
+        }
+
+       </div>
+
+    );
+  }
+}
+
+export default Dropdown;
