0efe9914cfd01bdac8efc0c0479c5a809dda7ca4
Sylvester Yue <sylvesteryue2301@gmail.com>
2020-01-12 03:21:25 -0800

    translate words

--- flask-backend/translate_api.py
+++ flask-backend/translate_api.py
@@ -1,10 +1,11 @@
-#set 
 from google.cloud import translate_v2 as translate
 client = translate.Client()
 
 def translate(word, target):
     #word = word.decode('utf-8')
     result = client.translate(word, target_language=target)
+
+    return result['translatedText']
 
     print(result['translatedText'])
 
